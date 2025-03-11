9180b553ef64e5dbf6bcbc3e165a520f311898a2
Sheikh Jabed <jabedbd2295@gmail.com>
2025-03-11 23:39:48 +0600

    New Root Layout added and functionalities completed

--- src/LayOuts/Root.jsx
+++ src/LayOuts/Root.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
-import { Toaster } from "react-hot-toast";
 import Footer from "../components/Footer/Footer";
+import { Toaster } from "react-hot-toast";
 
 const Root = () => {
   return (
